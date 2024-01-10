d669a51c983edd00b07dbc289fcb50e19e6eba72
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 <RSeymourJR04@outlook.com>
2024-01-10 13:49:43 -0500

    Alternating chat color.

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -48,7 +48,7 @@ const Chat = () => {
       <div className="chatInterface">
       <div className="convDisplay">
         {chat.map((item, index) => (
-          <div key={index} style={{backgroundColor: index % 2 === 0? 'green' : 'lime', textAlign: index % 2 === 0? 'right' : 'left', marginBottom: '20px'}}>
+          <div key={index} style={{backgroundColor: index % 2 === 0? '#DFDFDF' : '#000', color: index % 2 === 0? 'black' : 'white', textAlign: index % 2 === 0? 'right' : 'left', marginBottom: '20px', whiteSpace: 'pre-wrap'/* whiteSpace: 'pre-wrap' must be here for formatting */}}>
             <p><strong>{item.role}: </strong>{item.text}</p>
           </div>
         ))}
