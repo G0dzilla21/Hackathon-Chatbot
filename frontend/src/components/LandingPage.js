08c593bb38966dc644cd614c99e76ab053d73745
Kaylynnlaster <Kayannalynnlaster@gmail.com>
2024-01-10 13:28:02 -0600

    modal
    created modal to pop up chat

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,17 +1,41 @@
+import { useState } from "react";
+
 import "../style/LandingPage.css";
-import Chat from "./Chat";
+import DevChat from "./DevChat";
 
 const LandingPage = () => {
+  const [openDevChat, setDevChatOpen] = useState(false);
   return (
     <div>
       <header>
         <img src="logo.png" alt="Italian Trulli" />
         <nav>
-          <a href="#">Login</a>
-          <a href="#">Register</a>
+          <a href="#" className="firstNav">
+            Losep
+          </a>
+          <a href="#" className="secNav">
+            Rebjis
+          </a>
+          <a href="#" className="thirdNav">
+            Masfji
+          </a>
+          <a href="#" className="fourthNav">
+            Vajid
+          </a>
         </nav>
       </header>
-      <Chat />
+
+      <div>
+        <button
+          className="DevChatButton"
+          onClick={() => {
+            setDevChatOpen(true);
+          }}
+        >
+          <img src="logo.png" alt="Italian Trulli" />
+        </button>
+        {openDevChat && <DevChat closeDevChat={setDevChatOpen} />}
+      </div>
     </div>
   );
 };
